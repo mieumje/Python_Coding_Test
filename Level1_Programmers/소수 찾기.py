335d289a995807b402ac1caf83783d19ab473683
mieumje <66072832+mieumje@users.noreply.github.com>
2021-12-23 20:11:26 +0900

    Refactor : 소수 찾기
    is_prime 함수 for 문 범위 변경
    n 까지 > n의 제곱근 + 1
    
    정확성  테스트
    테스트 1 〉	통과 (0.00ms, 10.3MB)
    테스트 2 〉	통과 (0.11ms, 10.2MB)
    테스트 3 〉	통과 (0.48ms, 10.2MB)
    테스트 4 〉	통과 (0.89ms, 10.2MB)
    테스트 5 〉	통과 (0.38ms, 10.2MB)
    테스트 6 〉	통과 (8.02ms, 10.2MB)
    테스트 7 〉	통과 (1.61ms, 10.2MB)
    테스트 8 〉	통과 (4.55ms, 10.3MB)
    테스트 9 〉	통과 (7.95ms, 10.2MB)
    테스트 10 〉	통과 (671.06ms, 10.2MB)
    테스트 11 〉	통과 (3011.52ms, 10.3MB)
    테스트 12 〉	통과 (649.27ms, 10.2MB)
    효율성  테스트
    테스트 1 〉	통과 (2902.83ms, 10.1MB)
    테스트 2 〉	통과 (2767.97ms, 10.1MB)
    테스트 3 〉	실패 (시간 초과)
    테스트 4 〉	실패 (시간 초과)
    채점 결과
    정확성: 75.0
    효율성: 12.5
    합계: 87.5 / 100.0

--- Level1_Programmers/소수 찾기.py
+++ Level1_Programmers/소수 찾기.py
@@ -1,8 +1,11 @@
+import math
 def is_prime(n):
     if(n == 2):
         return True
 
-    for i in range(2, n):
+    sqrt_n = math.sqrt(n)
+    round_sqrt_n = math.ceil(sqrt_n)
+    for i in range(2, round_sqrt_n+1):
         if(n % i == 0):
             return False
     
@@ -17,5 +20,5 @@ def solution(n):
     
     return answer
 
-n = 5
+n = 10
 print(solution(n))
