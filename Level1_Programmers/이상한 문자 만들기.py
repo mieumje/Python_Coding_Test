e9bd99566d5fcc5ee75456c5bbcb5633fc8065a2
mieumje <66072832+mieumje@users.noreply.github.com>
2021-12-27 19:16:03 +0900

    Feat : 이상한 문자 만들기
    테스트 1
    입력값 〉	"try hello world"
    기댓값 〉	"TrY HeLlO WoRlD"
    실행 결과 〉	실행한 결괏값 "TrY HeLlO WoRlD "이(가) 기댓값 "TrY HeLlO WoRlD"와(과) 다릅니다.
    테스트 결과 (~˘▾˘)~
    1개 중 0개 성공
    
    마지막 반복문에서 answer에 " "이 들어감

---
+++ Level1_Programmers/이상한 문자 만들기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = ''
+    tmpArr = s.split(" ")
+    for i in tmpArr:
+        for x in range(0,len(i)):
+            if(x%2 == 0): answer += i[x].upper()
+            else: answer += i[x]
+        answer += " "
+    return answer
+
+s = "try hello world"
+print(solution(s))
