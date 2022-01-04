292f14720b77648f6b1c96d6eac8270535a24370
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-04 17:53:41 +0900

    Feat : 체육복
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.3MB)
    테스트 2 〉	통과 (0.01ms, 10.3MB)
    테스트 3 〉	통과 (0.01ms, 10.2MB)
    테스트 4 〉	통과 (0.01ms, 10.2MB)
    테스트 5 〉	통과 (0.01ms, 10.3MB)
    테스트 6 〉	통과 (0.00ms, 10.2MB)
    테스트 7 〉	실패 (0.01ms, 10.2MB)
    테스트 8 〉	통과 (0.00ms, 10.3MB)
    테스트 9 〉	통과 (0.00ms, 10.3MB)
    테스트 10 〉	통과 (0.01ms, 10.3MB)
    테스트 11 〉	통과 (0.00ms, 10.2MB)
    테스트 12 〉	통과 (0.00ms, 10.2MB)
    테스트 13 〉	실패 (0.00ms, 10.4MB)
    테스트 14 〉	실패 (0.00ms, 10.2MB)
    테스트 15 〉	통과 (0.00ms, 10.2MB)
    테스트 16 〉	통과 (0.00ms, 10.4MB)
    테스트 17 〉	통과 (0.00ms, 10.3MB)
    테스트 18 〉	통과 (0.00ms, 10.3MB)
    테스트 19 〉	통과 (0.00ms, 10.3MB)
    테스트 20 〉	통과 (0.00ms, 10.3MB)
    채점 결과
    정확성: 85.0
    합계: 85.0 / 100.0
    
    테스트 케이스 7, 13, 14 실패
    
    테스트 케이스 13, 14의 경우는 lost랑 reserve 배열 순서가 정렬되있지 않은 경우

---
+++ Level1_Programmers/체육복.py
@@ -0,0 +1,30 @@
+def solution(n, lost, reserve):    
+    answer = n - len(lost)  # 수업을 들을 수 있는 학생 수 = 전체 학생 수 - 체육복이 없는 학생 수
+    
+    for i in lost:          # 체육복이 없는 학생 중 여벌의 체육복이 있는 학생의 경우
+        if i in reserve:
+            lost.remove(i)
+            reserve.remove(i)
+            answer += 1
+    
+    for i in lost:          # 체육복이 없는 학생이 자신의 앞, 뒤 학생에게 체육복을 빌린 경우
+        if i-1 in reserve:
+            answer += 1
+            reserve.remove(i-1)
+            continue
+        elif i+1 in reserve:
+            answer += 1
+            reserve.remove(i+1)
+    
+    return answer
+
+# 입출력 예
+# n	lost	reserve	    return
+# 5	[2, 4]	[1, 3, 5]	5
+# 5	[2, 4]	[3]	        4
+# 3	[3]	    [1]	        2
+
+n = 5
+lost = [2,4]
+reserve = [1,3,5]
+print(solution(n,lost,reserve))
