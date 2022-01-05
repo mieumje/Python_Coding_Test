b46ef46e4dd76157646325073aa2a765ae92beb0
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-05 22:33:29 +0900

    Feat : 완주하지 못한 선수

---
+++ Level1_Programmers/K번째 수.py
@@ -0,0 +1,36 @@
+''' 
+배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+
+예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
+
+array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
+1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
+2에서 나온 배열의 3번째 숫자는 5입니다. 
+'''
+
+def solution(array, commands):
+    answer = []
+    
+    n = len(commands)
+    
+    for x in range(0,n):
+        i = commands[x][0] - 1
+        j = commands[x][1]
+        k = commands[x][2] - 1
+        
+        res = []
+        
+        for z in range(i,j):
+            res.append(array[z])
+        
+        result = sorted(res)
+        answer.append(result[k])
+        
+    return answer
+
+# 입출력 예
+# array	                commands	                        return
+# [1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
+array = [1, 5, 2, 6, 3, 7, 4]
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+print(solution(array, commands))
