2a745bf73c7494080d64655ec3895d37ad3e9888
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-06 21:37:16 +0900

    Refactor : 소수 만들기
    소수를 판별하는 함수에서 반복문 범위 수정
    기존에 넘겨받은 숫자 전체를 반복했는데 제곱근까지만 반복
    
    정확성  테스트
    테스트 1 〉	통과 (2.28ms, 10.3MB)
    테스트 2 〉	통과 (3.29ms, 10.4MB)
    테스트 3 〉	통과 (0.59ms, 10.2MB)
    테스트 4 〉	통과 (0.73ms, 10.3MB)
    테스트 5 〉	통과 (3.34ms, 10.3MB)
    테스트 6 〉	통과 (4.94ms, 10.4MB)
    테스트 7 〉	통과 (0.18ms, 10.3MB)
    테스트 8 〉	통과 (12.24ms, 11.2MB)
    테스트 9 〉	통과 (1.54ms, 10.3MB)
    테스트 10 〉	통과 (11.30ms, 10.9MB)
    테스트 11 〉	통과 (0.09ms, 10.3MB)
    테스트 12 〉	통과 (0.04ms, 10.2MB)
    테스트 13 〉	통과 (0.11ms, 10.3MB)
    테스트 14 〉	통과 (0.05ms, 10.4MB)
    테스트 15 〉	통과 (0.04ms, 10.2MB)
    테스트 16 〉	통과 (13.44ms, 11.2MB)
    테스트 17 〉	통과 (12.57ms, 11.3MB)
    테스트 18 〉	통과 (0.09ms, 10.2MB)
    테스트 19 〉	통과 (0.01ms, 10.2MB)
    테스트 20 〉	통과 (17.63ms, 11.3MB)
    테스트 21 〉	통과 (16.98ms, 11.4MB)
    테스트 22 〉	통과 (2.30ms, 10.4MB)
    테스트 23 〉	통과 (0.01ms, 10.3MB)
    테스트 24 〉	통과 (13.35ms, 11.2MB)
    테스트 25 〉	통과 (13.64ms, 11.3MB)
    테스트 26 〉	통과 (0.02ms, 10.3MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- Level1_Programmers/소수 만들기.py
+++ Level1_Programmers/소수 만들기.py
@@ -3,28 +3,24 @@
 nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return
 """
 from itertools import combinations
-
+import math
 def is_prime(num):
-    for i in range(2, num):
-        check_prime = 0
-        if num % i == 0:
-            check_prime = 1 # 소수가 아님
-            break
+    check_prime = True
+    N = int(math.sqrt(num))
     
-    if check_prime != 0: # 소수가 아닐 때 0 return
-        return 0
-    else:               # 소수이면 1 return
-        return 1
+    for i in range(2,N+1):
+        if(num % i == 0):
+            check_prime = False
+            return False
+    return check_prime
 
 def solution(nums):
     answer = 0
-    
-    b = list(combinations(nums, 3)) # nums 중 3개로 조합할 수 있는 숫자
-    
-    for i in range(0, len(b)):
-        x = b[i][0] + b[i][1] + b[i][2]
-        result = is_prime(x)
-        answer += result
+    tmp_list = list(combinations(nums,3))
+    tmp_sum = 0
+    for i in range(0, len(tmp_list)):
+        tmp_sum = tmp_list[i][0] + tmp_list[i][1] + tmp_list[i][2]
+        if(is_prime(tmp_sum)) : answer += 1
         
     return answer
 
@@ -32,3 +28,6 @@ def solution(nums):
 # nums	        result
 # [1,2,3,4]	    1
 # [1,2,7,6,4]	4
+
+nums = [1,2,3,4]
+print(solution(nums))
