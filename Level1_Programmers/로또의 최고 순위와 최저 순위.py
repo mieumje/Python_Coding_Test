5ca2cdcdede91b83cfb4fbee6bd5aa179e3fb0af
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-07 17:15:20 +0900

    Refactor : 로또의 최고 순위와 최저 순위
    불필요한 변수 제거
    반복문 한번만 실행하도록 수정
    
    정확성  테스트
    테스트 1 〉	통과 (0.00ms, 10.2MB)
    테스트 2 〉	통과 (0.00ms, 10.3MB)
    테스트 3 〉	통과 (0.00ms, 10.2MB)
    테스트 4 〉	통과 (0.00ms, 10.2MB)
    테스트 5 〉	통과 (0.00ms, 10.2MB)
    테스트 6 〉	통과 (0.00ms, 10.3MB)
    테스트 7 〉	통과 (0.00ms, 10.2MB)
    테스트 8 〉	통과 (0.00ms, 10.2MB)
    테스트 9 〉	통과 (0.00ms, 10.3MB)
    테스트 10 〉	통과 (0.00ms, 10.2MB)
    테스트 11 〉	통과 (0.00ms, 10.3MB)
    테스트 12 〉	통과 (0.00ms, 10.4MB)
    테스트 13 〉	통과 (0.00ms, 10.2MB)
    테스트 14 〉	통과 (0.00ms, 10.2MB)
    테스트 15 〉	통과 (0.00ms, 10.2MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- Level1_Programmers/로또의 최고 순위와 최저 순위.py
+++ Level1_Programmers/로또의 최고 순위와 최저 순위.py
@@ -7,32 +7,23 @@ def ratings(cnt):
     else: return 1          # 순위 : 1, 6개 번호가 모두 일치
     
 def solution(lottos, win_nums):
-    cnt = 0
+    low_cnt = 0
+    high_cnt = 0
     for i in lottos:
         if i in win_nums:
-            cnt += 1
-    low_rating = cnt
-    
-    for i in lottos:
+            low_cnt += 1
+            high_cnt += 1
         if i == 0:
-            cnt += 1
-    high_rating = cnt
-    
-    low_rating, high_rating = ratings(low_rating), ratings(high_rating)
+            high_cnt += 1
+            
+    high_rating, low_rating = ratings(high_cnt), ratings(low_cnt) 
     return [high_rating, low_rating]
-# 순위	    당첨 내용
-# 1	        6개 번호가 모두 일치
-# 2	        5개 번호가 일치
-# 3	        4개 번호가 일치
-# 4	        3개 번호가 일치
-# 5	        2개 번호가 일치
-# 6(낙첨)	그 외
 
 # 입출력 예
 # lottos	            win_nums	                result
 # [44, 1, 0, 0, 31, 25]	[31, 10, 45, 1, 6, 19]	    [3, 5]
 # [0, 0, 0, 0, 0, 0]	[38, 19, 20, 40, 15, 25]	[1, 6]
 # [45, 4, 35, 20, 3, 9]	[20, 9, 3, 45, 4, 35]	    [1, 1]
-lottos = [45, 4, 35, 20, 3, 9]
-win_nums = [20, 9, 3, 45, 4, 35]
+lottos = [44, 1, 0, 0, 31, 25]
+win_nums = [31, 10, 45, 1, 6, 19]
 print(solution(lottos,win_nums))
