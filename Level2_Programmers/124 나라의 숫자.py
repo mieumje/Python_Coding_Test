f9ccb62b13e331293e56bed1cf6e38135d723741
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-10 21:37:23 +0900

    Feat : 124 나라의 숫자
    3번씩 반복, 3으로 나눈 나머지를 활용
    나머지가 1, 2인경우 그대로 더하기
    나머지가 0인 경우 3+1만큼 더하기
    답에 3+1만큼 더했기에 몫에서 -1해주기
    몫이 0이 아닐때까지 반복
    
    정확성  테스트
    테스트 1 〉	통과 (0.00ms, 10.3MB)
    테스트 2 〉	통과 (0.00ms, 10.2MB)
    테스트 3 〉	통과 (0.00ms, 10.2MB)
    테스트 4 〉	통과 (0.00ms, 10.2MB)
    테스트 5 〉	통과 (0.01ms, 10.1MB)
    테스트 6 〉	통과 (0.01ms, 10.2MB)
    테스트 7 〉	통과 (0.01ms, 10.2MB)
    테스트 8 〉	통과 (0.01ms, 10.2MB)
    테스트 9 〉	통과 (0.01ms, 10.3MB)
    테스트 10 〉	통과 (0.01ms, 10.3MB)
    테스트 11 〉	통과 (0.01ms, 10.2MB)
    테스트 12 〉	통과 (0.01ms, 10.2MB)
    테스트 13 〉	통과 (0.01ms, 10.2MB)
    테스트 14 〉	통과 (0.01ms, 10.2MB)
    효율성  테스트
    테스트 1 〉	통과 (0.01ms, 10.2MB)
    테스트 2 〉	통과 (0.01ms, 10.1MB)
    테스트 3 〉	통과 (0.01ms, 10MB)
    테스트 4 〉	통과 (0.01ms, 10.1MB)
    테스트 5 〉	통과 (0.01ms, 10.2MB)
    테스트 6 〉	통과 (0.01ms, 10.2MB)
    채점 결과
    정확성: 70.0
    효율성: 30.0
    합계: 100.0 / 100.0

---
+++ Level2_Programmers/124 나라의 숫자.py
@@ -0,0 +1,36 @@
+# 10진법	124 나라	     10진법	 124 나라
+# 1	        1	            6	    14
+# 2	        2	            7	    21
+# 3	        4	            8	    22
+# 4	        11	            9	    24
+# 5	        12	            10	    41
+#                           11      42
+#                           13      111
+# 3으로 나누었을 때
+# 1인 경우 > +1
+# 2인 경우 > +2
+# 3인 경우 > +3+1, +1한 만큼 n을 -1
+def solution(n):
+    answer = ''
+    result = []
+    while (n > 0):
+        mod = n % 3
+        if mod == 0:
+            mod = 4 # mod + 1, 3의 경우 mod가 0이라서 4로 설정
+            n = n - 1
+        result.append(str(mod))
+        n = n // 3
+    answer = ''.join(result[::-1])
+    return answer
+
+# 제한사항
+# n은 500,000,000이하의 자연수 입니다.
+
+# 입출력 예
+# n	    result
+# 1	    1
+# 2	    2
+# 3	    4
+# 4	    11
+for i in range(1, 21):
+    print(solution(i))
