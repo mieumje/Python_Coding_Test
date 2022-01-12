4d86ad4ed3efb882c3f9560e01063a59ab7fa114
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-12 17:09:27 +0900

    Feat : 타깃 넘버
    정확성  테스트
    테스트 1 〉	통과 (168.27ms, 50.1MB)
    테스트 2 〉	통과 (167.37ms, 49.4MB)
    테스트 3 〉	통과 (0.16ms, 10.1MB)
    테스트 4 〉	통과 (0.63ms, 10.3MB)
    테스트 5 〉	통과 (5.14ms, 11.1MB)
    테스트 6 〉	통과 (0.32ms, 10.2MB)
    테스트 7 〉	통과 (0.16ms, 10.2MB)
    테스트 8 〉	통과 (1.32ms, 10.4MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

---
+++ Level2_Programmers/타깃 넘버.py
@@ -0,0 +1,34 @@
+"""
+n개의 음이 아닌 정수가 있습니다. 
+이 수를 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 
+예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 
+숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
+"""
+
+def solution(numbers, target):
+    answer = 0
+    arr = [0]   # 첫 번째 인덱스 요소를 더하고, 빼기 위한 0
+    for i in numbers:
+        tmp = []    
+        for j in arr:   # 더하기, 빼기 연산을 한 결과가 담긴 배열을 탐색
+            tmp.append(j + i)   # 해당 인덱스 값에 더하기
+            tmp.append(j - 1)   # 해당 인덱스 값에 빼기
+        arr = tmp               # 연산 후 arr 갱신
+    for i in arr:               # 더하기, 빼기의 모든 연산의 경우의 수를 탐색
+        if i == target:         # 타겟 넘버와 같을 때 answer += 1
+            answer += 1
+    return answer
+
+# 입출력 예
+# numbers	        target	return
+# [1, 1, 1, 1, 1]	3	    5
+numbers = [1,1,1,1,1]
+target = 3
+print(solution(numbers,target))
