3c4eb9c6ba93a8dac428ff8c6c312be9ecc5edfd
mieumje <66072832+mieumje@users.noreply.github.com>
2022-01-15 16:49:33 +0900

    Refactor : 괄호 변환
    문제 풀이를 위한 설명을 토대로 로직 작성
    
    p에 대하여 u, v를 분리하고
    분리된 u, v를 올바른 괄호 문자열이 되는지 체크
    
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.3MB)
    테스트 2 〉	통과 (0.01ms, 10.2MB)
    테스트 3 〉	통과 (0.01ms, 10.3MB)
    테스트 4 〉	통과 (0.01ms, 10.3MB)
    테스트 5 〉	통과 (0.01ms, 10.3MB)
    테스트 6 〉	통과 (0.01ms, 10.3MB)
    테스트 7 〉	통과 (0.01ms, 10.3MB)
    테스트 8 〉	통과 (0.01ms, 10.3MB)
    테스트 9 〉	통과 (0.02ms, 10.2MB)
    테스트 10 〉	통과 (0.01ms, 10.3MB)
    테스트 11 〉	통과 (0.03ms, 10.3MB)
    테스트 12 〉	통과 (0.03ms, 10.3MB)
    테스트 13 〉	통과 (0.04ms, 10.3MB)
    테스트 14 〉	통과 (0.06ms, 10.3MB)
    테스트 15 〉	통과 (0.08ms, 10.2MB)
    테스트 16 〉	통과 (0.14ms, 10.3MB)
    테스트 17 〉	통과 (0.13ms, 10.3MB)
    테스트 18 〉	통과 (0.20ms, 10.4MB)
    테스트 19 〉	통과 (0.30ms, 10.3MB)
    테스트 20 〉	통과 (0.25ms, 10.3MB)
    테스트 21 〉	통과 (0.22ms, 10.3MB)
    테스트 22 〉	통과 (0.11ms, 10.3MB)
    테스트 23 〉	통과 (0.36ms, 10.3MB)
    테스트 24 〉	통과 (0.16ms, 10.3MB)
    테스트 25 〉	통과 (0.14ms, 10.3MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- Level2_Programmers/괄호 변환.py
+++ Level2_Programmers/괄호 변환.py
@@ -1,34 +1,66 @@
-def check_correction(p):
-    tmp = p.split("()")
-    s = ""
-    for i in tmp:
-        s += "".join(i)
-    if(s == "()"):
-        return True
-    else:
-        return False
-    
+"""
+문제 풀이 로직
+
+1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다. 
+2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다. 
+3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다. 
+  3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다. 
+4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다. 
+  4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다. 
+  4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
+  4-3. ')'를 다시 붙입니다. 
+  4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
+  4-5. 생성된 문자열을 반환합니다.
+
+"""
+def divide_uv(p):
+    left_count, right_count = 0, 0
+    for i in range(0, len(p)):
+        if(p[i] == "("):
+            left_count += 1
+        else:
+            right_count += 1
+        
+        if left_count == right_count:
+            return p[:i+1], p[i+1:]    
+            
+
+def check_correction(u):
+    tmp = []
+    for i in u:
+        if(i == "("):
+            tmp.append(i)
+        else:
+            if not tmp:
+                return False
+            else:
+                tmp.pop()
+    return True
+
 def solution(p):
     answer = ''
-    if p == "":
+    if not p:                       # 1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다. 
         return ""
-    if check_correction(p):
-        return p
-    else:
-        tmp = p.split("()")
-        for i in tmp:
-            if i:
-                l = len(i)
-                for i in range(0, l):
-                    if(i < l // 2):
-                        answer += "("
-                    else:
-                        answer += ")"
+    u,v = divide_uv(p)              # 2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다.
+
+    if check_correction(u):         # 3. 문자열 u가 "올바른 괄호 문자열"
+        return u + solution(v)      # 문자열 v에 대해 1단계부터 다시 수행합니다.
+    else:                           # 4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다. 
+        answer += "("               # 4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다.
+        answer += solution(v)       # 4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다.
+        answer += ")"               # 4-3. ')'를 다시 붙입니다.
+        for i in range(1, len(u)-1):# 4-4. u의 첫 번째와 마지막 문자를 제거하고,
+            if u[i] == "(":         # 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다.
+                answer += ")"
             else:
-                answer += "()"
-        return answer
+                answer += "("
+        return answer               # 4-5. 생성된 문자열을 반환합니다.
 
-
+# 입출력 예
+# p	            result
+# "(()())()"	"(()())()"
+# ")("	        "()"
+# "()))((()"	"()(())()"
 p = "()))((()"
 print(solution(p))
 
