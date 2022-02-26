54c97f13dd2ab855721b499d5f514493aca385e4
mieumje <66072832+mieumje@users.noreply.github.com>
2022-02-26 20:44:54 +0900

    Refactor : 거리두기 확인하기
    BFS 활용
    
    실행시간 초과, 반복문에 문제가 있는 듯 함

--- Level2_Programmers/거리두기 확인하기.py
+++ Level2_Programmers/거리두기 확인하기.py
@@ -1,65 +1,72 @@
-# p 기준 상하좌우 p의 개수 1 이상이면 0
-# o 기준 상하좌우 p의 개수 2 이상이면 0
+from queue import Queue
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(row, col, place):
+    visited = [[False for _ in range(5)] for _ in range(5)]
+    q = Queue()
+    visited[row][col] = 1
+    q.put((row, col, 0))
+
+    while q:
+        cur = q.get()
+        cur_x = cur[0]
+        cur_y = cur[1]
+
+        if cur[2] > 2:
+            continue
+        if cur[2] != 0 and place[cur_x][cur_y] == 'P':
+            return False
+
+        for i in range(4):
+            nx = cur_x + dx[i]
+            ny = cur_y + dy[i]
+
+            if nx < 0 or nx > 4 or ny < 0 or ny > 4:
+                continue
+            if visited[nx][ny]:
+                continue
+            if place[nx][ny] == 'X':
+                continue
+
+            visited[nx][ny] = 1
+            q.put((nx, ny, cur[2] + 1))
+    return True
+
+
+def check_distance(place):
+    for row in range(5):
+        for col in range(5):
+            if place[row][col] == 'P':
+                if bfs(row, col, place) == False:
+                    return False
+    return True
+
 
 def solution(places):
     answer = []
-    
-    for i in places:
-        arr = []
-        for j in i:
-            tmp = []
-            for x in j:
-                tmp.append(x)
-            arr.append(tmp)
-
-        for x in range(0, len(arr)):
-            for y in range(0, len(arr[x])):
-                check_distance = 1
-                if y == 0:
-                    left = ''
-                else:
-                    left = arr[x][y-1]
-                if x == 4:
-                    bottom = ''
-                else:
-                    bottom = arr[x+1][y]
-                if x == 0:
-                    top = ''
-                else:
-                    top = arr[x-1][y]
-                if y == 4:
-                    right = ''
-                else:
-                    right = arr[x][y+1]
-                cur = arr[x][y]
-                left_right_top_bottom = [left, right, top, bottom]
-                cnt_p = left_right_top_bottom.count('P')
-                
-                if cur == 'P':
-                    if cnt_p >= 1:
-                        check_distance = 0
-                        break
-                if cur == 'O':
-                    if cnt_p >= 2:
-                        check_distance = 0
-                        break
-        answer.append(check_distance)
-                
-            
+    for place in places:
+        if check_distance(place):
+            answer.append(1)
+        else:
+            answer.append(0)
     return answer
 
 # 입출력 예
 # places	                                        result
-# [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], 
-#  ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
-#  ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], 
-#  ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], 
+# [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
+#  ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
+#  ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"],
+#  ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
 #  ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]	[1, 0, 1, 1, 1]
 
-places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], 
- ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
- ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], 
- ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], 
- ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
+
+places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
+          ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
+          ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"],
+          ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
+          ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
 
 print(solution(places))
