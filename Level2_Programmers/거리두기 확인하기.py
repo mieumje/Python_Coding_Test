2400b6568648db10f0a7b101b6fc0bc25a468e40
mieumje <66072832+mieumje@users.noreply.github.com>
2022-02-27 17:33:14 +0900

    Refactor : 거리두기 확인하기
    deque로 변환
    while문 조건
    
    정확성  테스트
    테스트 1 〉	통과 (0.18ms, 10.4MB)
    테스트 2 〉	통과 (0.07ms, 10.4MB)
    테스트 3 〉	통과 (0.07ms, 10.2MB)
    테스트 4 〉	통과 (0.06ms, 10.4MB)
    테스트 5 〉	통과 (0.05ms, 10.4MB)
    테스트 6 〉	통과 (0.10ms, 10.1MB)
    테스트 7 〉	통과 (0.07ms, 10.2MB)
    테스트 8 〉	통과 (0.09ms, 10.2MB)
    테스트 9 〉	통과 (0.09ms, 10.3MB)
    테스트 10 〉	통과 (0.05ms, 10.1MB)
    테스트 11 〉	통과 (0.07ms, 10.2MB)
    테스트 12 〉	통과 (0.06ms, 10.2MB)
    테스트 13 〉	통과 (0.08ms, 10.2MB)
    테스트 14 〉	통과 (0.06ms, 10.1MB)
    테스트 15 〉	통과 (0.05ms, 10.3MB)
    테스트 16 〉	통과 (0.07ms, 10.2MB)
    테스트 17 〉	통과 (0.08ms, 10.2MB)
    테스트 18 〉	통과 (0.06ms, 10.1MB)
    테스트 19 〉	통과 (0.08ms, 10.2MB)
    테스트 20 〉	통과 (0.08ms, 10.2MB)
    테스트 21 〉	통과 (0.05ms, 10.3MB)
    테스트 22 〉	통과 (0.07ms, 10.2MB)
    테스트 23 〉	통과 (0.02ms, 10.2MB)
    테스트 24 〉	통과 (0.03ms, 10.3MB)
    테스트 25 〉	통과 (0.02ms, 10.3MB)
    테스트 26 〉	통과 (0.02ms, 10.1MB)
    테스트 27 〉	통과 (0.06ms, 10.2MB)
    테스트 28 〉	통과 (0.07ms, 10.3MB)
    테스트 29 〉	통과 (0.06ms, 10.2MB)
    테스트 30 〉	통과 (0.04ms, 10.4MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- Level2_Programmers/거리두기 확인하기.py
+++ Level2_Programmers/거리두기 확인하기.py
@@ -1,4 +1,5 @@
-from queue import Queue
+#from queue import Queue
+from collections import deque
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -6,18 +7,20 @@ dy = [0, 0, -1, 1]
 
 def bfs(row, col, place):
     visited = [[False for _ in range(5)] for _ in range(5)]
-    q = Queue()
+    # q = Queue()
+    q = deque()
     visited[row][col] = 1
-    q.put((row, col, 0))
-
-    while q:
-        cur = q.get()
+    # q.put((row, col, 0))
+    q.append((row, col, 0))
+    while len(q) > 0:
+        cur = q.popleft()
         cur_x = cur[0]
         cur_y = cur[1]
+        dist = cur[2]
 
-        if cur[2] > 2:
+        if dist > 2:
             continue
-        if cur[2] != 0 and place[cur_x][cur_y] == 'P':
+        if dist != 0 and place[cur_x][cur_y] == 'P':
             return False
 
         for i in range(4):
@@ -26,13 +29,14 @@ def bfs(row, col, place):
 
             if nx < 0 or nx > 4 or ny < 0 or ny > 4:
                 continue
-            if visited[nx][ny]:
+            if visited[nx][ny] == True:
                 continue
             if place[nx][ny] == 'X':
                 continue
 
-            visited[nx][ny] = 1
-            q.put((nx, ny, cur[2] + 1))
+            visited[nx][ny] = True
+            q.append((nx, ny, dist + 1))
+
     return True
 
 
@@ -70,3 +74,14 @@ places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
           ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
 
 print(solution(places))
+# q = deque()
+# print(len(q))
+# q.append((1, 2, 3))
+# print(q, len(q))
+# q.append((0, 4, 2))
+# print(q, len(q))
+# x = q.popleft()
+# print(x[0], x[2])
+# print(q, len(q))
+# q.popleft()
+# print(q, len(q))
