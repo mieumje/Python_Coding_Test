c51e6a357280d33ada982401ec70e1b0b0187a51
mieumje <66072832+mieumje@users.noreply.github.com>
2022-03-06 19:49:47 +0900

    Refcator : [1차] 뉴스 클러스터링
    collections 모듈 Counter 패키지 사용

--- Level2_Programmers/[1차] 뉴스 클러스터링.py
+++ Level2_Programmers/[1차] 뉴스 클러스터링.py
@@ -1,4 +1,6 @@
-import math
+from collections import Counter
+
+
 def make_multiset(s):
     tmp = []
     for i in range(0, len(s) - 1):
@@ -7,40 +9,25 @@ def make_multiset(s):
             tmp.append(tmp_element.upper())
     return tmp
 
+
 def solution(str1, str2):
-    answer = 0
-    J = 0
-    
     # 문자열 str1, str2를 다중집합으로 만들기
     str1_multiset = make_multiset(str1)
     str2_multiset = make_multiset(str2)
-    if not list(set(str1_multiset) & set(str2_multiset)): # str1과 str2의 교집합이 없는 경우 자카드 유사도는 1
-        J = 1
-    else:
-        b = list(set(str1_multiset) & set(str2_multiset))
-        str1_cnt, str2_cnt = 0, 0
-        for i in b:
-            str1_cnt += str1_multiset.count(i)
-            str2_cnt += str2_multiset.count(i)
-        intersection = min(str1_cnt,str2_cnt)
-        if str1_cnt == str2_cnt:
-            cnt = 0
-            for i in str1_multiset:
-                for j in str2_multiset:
-                    if i == j:
-                        cnt += 1
-                        break
-            union = len(str1_multiset) + len(str2_multiset) - cnt
-        else:
-            union = max(str1_cnt,str2_cnt)
-        J = intersection / union
-    answer = math.floor(J * 65536)
-    return answer
+    Counter1 = Counter(str1_multiset)
+    Counter2 = Counter(str2_multiset)
 
+    a = list((Counter1 & Counter2).elements())  # inter
+    b = list((Counter1 | Counter2).elements())  # union
+
+    if len(a) == 0 and len(b) == 0:
+        return 65536
+    else:
+        return int((len(a) / len(b)) * 65536)
 
 
 # 출력 형식
-# 입력으로 들어온 두 문자열의 자카드 유사도를 출력한다. 유사도 값은 0에서 1 사이의 실수이므로, 
+# 입력으로 들어온 두 문자열의 자카드 유사도를 출력한다. 유사도 값은 0에서 1 사이의 실수이므로,
 # 이를 다루기 쉽도록 65536을 곱한 후에 소수점 아래를 버리고 정수부만 출력한다.
 
 # 예제 입출력
@@ -49,7 +36,16 @@ def solution(str1, str2):
 # handshake	shake hands	65536
 # aa1+aa2	AAAA12	    43690
 # E=M*C^2	e=m*c^2	    65536
-
+str1 = 'FRANCE'
+str2 = 'french'
+print(solution(str1, str2))
+str1 = 'handshake'
+str2 = 'shake hands'
+print(solution(str1, str2))
+str1 = 'aa1+aa2'
+str2 = 'AAAA12'
+print(solution(str1, str2))
 str1 = 'E=M*C^2'
 str2 = 'e=m*c^2'
-print(solution(str1,str2))
+print(solution(str1, str2))
+print((0/3)*65536)
