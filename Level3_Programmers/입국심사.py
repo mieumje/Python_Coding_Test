aa3ce64facd165c4f83af7b76dd80b4918a2e87f
mieumje <66072832+mieumje@users.noreply.github.com>
2022-03-30 22:58:49 +0900

    Refactor: 입국심사 - 성공
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 28.3MB)
    테스트 2 〉	통과 (0.10ms, 28.3MB)
    테스트 3 〉	통과 (3.65ms, 28.1MB)
    테스트 4 〉	통과 (348.90ms, 32.4MB)
    테스트 5 〉	통과 (496.89ms, 32.6MB)
    테스트 6 〉	통과 (337.12ms, 32.3MB)
    테스트 7 〉	통과 (651.00ms, 32.5MB)
    테스트 8 〉	통과 (645.92ms, 32.3MB)
    테스트 9 〉	통과 (0.03ms, 28.3MB)
    채점 결과
    정확성: 100.0
    합계: 100.0 / 100.0

--- Level3_Programmers/입국심사.py
+++ Level3_Programmers/입국심사.py
@@ -15,22 +15,19 @@ def solution(n, times):
     right = times[-1] * n
 
     while left <= right:
-        mid = math.trunc((left + right) / 2)
-
+        #mid = math.trunc((left + right) / 2)
+        mid = (left + right) // 2
         sum = accumulate(mid, times)
-        print(left, mid, right, sum)
         if sum >= n:
             right = mid - 1
         else:
             left = mid + 1
-
-    return mid + 1
+    return left
 
 
 # 입출력 예
 # n	        times	    return
 # 6	        [7, 10]	    28
-
 n = 6
 times = [7, 10]
 print(solution(n, times))
