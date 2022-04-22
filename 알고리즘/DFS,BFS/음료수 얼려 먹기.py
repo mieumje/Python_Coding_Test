bcbd7b8e95e6de24588e19b1b7e3e8dd84a86caa
mieumje <66072832+mieumje@users.noreply.github.com>
2022-04-22 15:53:40 +0900

    DFS,BFS : 음료수 얼려 먹기(DFS)

--- 알고리즘/DFS,BFS/음료수 얼려 먹기.py
+++ 알고리즘/DFS,BFS/음료수 얼려 먹기.py
@@ -4,25 +4,30 @@ graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
-def dfs(x,y):
-  if x < 0 or x >= n or y < 0 or y >= m:
+dx = [-1, +1, 0, 0]
+dy = [0, 0, -1, +1]
+
+
+def dfs(x, y):
+    if x < 0 or x >= n or y < 0 or y >= m:
+        return False
+
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+
+        # 좌, 우, 상, 하 [x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]
+        for i in range(0, 4):
+            nx, ny = x + dx[i], y + dy[i]
+            dfs(nx, ny)
+
+        return True
     return False
 
-  if graph[x][y] == 0:
-    graph[x][y] = 1
-    
-    # 좌, 우, 상, 하 [x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]
-    dfs(x - 1, y)
-    dfs(x + 1, y)
-    dfs(x, y - 1)
-    dfs(x, y + 1)
-    return True
-  return False
 
 answer = 0
 
 for x in range(n):
-  for y in range(m):
-    if dfs(x,y) == True:
-      answer = answer + 1
+    for y in range(m):
+        if dfs(x, y) == True:
+            answer = answer + 1
 print(answer)
