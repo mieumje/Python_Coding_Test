f6cdc263dfb0d9300b84d188fa42215c512fad64
mieumje <66072832+mieumje@users.noreply.github.com>
2022-09-08 18:34:19 +0900

    feat: 3번(오답풀이)  2개 이하로 다 비트

--- 알고리즘/0908/3.py
+++ 알고리즘/0908/3.py
@@ -1,36 +1,22 @@
 def solution(numbers):
     answer = []
     for i in numbers:
-      compare_number = i
-      while True:
-        xor_number = bin(i ^ compare_number)
-        bin_number = xor_number.split('b')
-        cnt = bin_number[1].count('1')
-        if cnt == 1 or cnt == 2:
-          answer.append(compare_number)
-          break
-        else:
-          compare_number = compare_number + 1
+      bin_number = list('0' + bin(i)[2:])
+      
+      idx = ''.join(bin_number).rfind('0')
+      print(i, bin_number, idx)
+      bin_number[idx] = '1'
+      
+      if i % 2 == 1:
+        bin_number[idx + 1] = '0'
+        
+      answer.append(int(''.join(bin_number), 2))
+      
     return answer
 
-# print(bin(7))
-# print('----')
-# print(bin(7 ^ 8), bin(7 ^ 8).split('0b'))
-# print(bin(7 ^ 9), bin(7 ^ 8).split('0b'))
-# print(bin(7 ^ 10), bin(7 ^ 8).split('0b'))
-# print(bin(7 ^ 11), bin(7 ^ 8).split('0b'))
-# print(bin(7 ^ 12), bin(7 ^ 8).split('0b'))
-# print(bin(7 ^ 13), bin(7 ^ 8).split('0b'))
-
-# tmp = bin(7 ^ 13)
-# for i in tmp:
-#   print(i)
-  
-# arr = tmp.split('b')
-
-# print(arr[1], arr[1].count('1'))
-
-# numbers   result
-# [2, 7]    [3, 11]
-numbers = [2, 7]
+numbers = [2, 4, 7]
 print(solution(numbers))
+
+# 가장 오른쪽에 있는 0을 1로 바꾼다 => 0의 위치 = idx
+# 홀수 인 경우 idx + 1을 0으로 바꾼다
+# 해당 2진수를 10진수로 변경
