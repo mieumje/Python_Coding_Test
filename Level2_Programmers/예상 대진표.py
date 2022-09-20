50452cd6ad67557c03b2689753ef3fc80e05e126
mieumje <66072832+mieumje@users.noreply.github.com>
2022-09-20 19:15:48 +0900

    feat: 예상 대진표(실패) 2차
    정확성  테스트
    테스트 1 〉	통과 (0.00ms, 9.95MB)
    테스트 2 〉	통과 (0.01ms, 10.1MB)
    테스트 3 〉	통과 (0.01ms, 10.3MB)
    테스트 4 〉	통과 (0.01ms, 10.2MB)
    테스트 5 〉	통과 (0.01ms, 10.3MB)
    테스트 6 〉	통과 (0.01ms, 10.2MB)
    테스트 7 〉	실패 (0.01ms, 10.1MB)
    테스트 8 〉	통과 (0.01ms, 10.1MB)
    테스트 9 〉	실패 (0.01ms, 10.2MB)
    테스트 10 〉	통과 (0.01ms, 10.1MB)
    테스트 11 〉	통과 (0.01ms, 10.1MB)
    테스트 12 〉	통과 (0.01ms, 10.1MB)
    테스트 13 〉	통과 (0.01ms, 10MB)
    테스트 14 〉	통과 (0.01ms, 10.3MB)
    테스트 15 〉	통과 (0.01ms, 10.2MB)
    테스트 16 〉	통과 (0.01ms, 10.1MB)
    테스트 17 〉	통과 (0.01ms, 10.3MB)
    테스트 18 〉	통과 (0.01ms, 10.2MB)
    테스트 19 〉	통과 (0.01ms, 10.1MB)
    테스트 20 〉	통과 (0.01ms, 10.1MB)
    테스트 21 〉	통과 (0.01ms, 10.1MB)
    테스트 22 〉	통과 (0.01ms, 10.1MB)
    테스트 23 〉	통과 (0.01ms, 10.3MB)
    테스트 24 〉	통과 (0.01ms, 10.1MB)
    테스트 25 〉	실패 (시간 초과)
    테스트 26 〉	통과 (0.00ms, 10.3MB)
    테스트 27 〉	실패 (0.01ms, 10.3MB)
    테스트 28 〉	통과 (0.01ms, 10.2MB)
    테스트 29 〉	통과 (0.01ms, 10.2MB)
    테스트 30 〉	통과 (0.01ms, 10.1MB)
    테스트 31 〉	통과 (0.01ms, 10MB)
    테스트 32 〉	통과 (0.01ms, 10.1MB)
    테스트 33 〉	실패 (0.01ms, 10.4MB)
    테스트 34 〉	통과 (0.01ms, 10.1MB)
    채점 결과
    정확성: 85.3
    합계: 85.3 / 100.0

--- Level2_Programmers/예상 대진표.py
+++ Level2_Programmers/예상 대진표.py
@@ -2,6 +2,9 @@ import math
 
 def solution(n,a,b):
   answer = 0
+  if b % 2 == 0 and b - 1 == a:
+    answer += 1
+    return answer
   while True:
     a = math.ceil(a / 2)
     b = math.ceil(b / 2)
