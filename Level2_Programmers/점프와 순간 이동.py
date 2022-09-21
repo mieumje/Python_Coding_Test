bf0b4db4a10f18d79b50a51a1063f05eed7c4165
mieumje <66072832+mieumje@users.noreply.github.com>
2022-09-21 18:34:20 +0900

    feat: 점프와 순간 이동(2차 시도 성공)
    효율성 실패 했던 코드 다시 돌려보니 성공
    
    bottom up 방식 -> top down 방식으로 교체 후 성공

--- Level2_Programmers/점프와 순간 이동.py
+++ Level2_Programmers/점프와 순간 이동.py
@@ -1,6 +1,19 @@
-def solution(n):
+def solution1(n):
   ans = 0
   
+  while n > 0:
+    if n % 2 == 0:
+      n /= 2
+    else:
+      n -= 1
+      ans += 1
+      
+  return ans
+
+
+def solution2(n):
+  ans = 0
+
   while True:
     if n <= 2: break
     if n % 2 == 0:
@@ -8,9 +21,8 @@ def solution(n):
     else:
       n -= 1
       ans += 1
-      
-  return ans + 1
 
+  return ans + 1
 # N	    result
 # 5	    2
 # 6	    2
