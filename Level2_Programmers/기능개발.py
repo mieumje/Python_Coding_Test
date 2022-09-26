00855269b64f4538ca8f824bbb03cb5c99936da8
mieumje <66072832+mieumje@users.noreply.github.com>
2022-09-26 23:40:19 +0900

    feat: 기능 개발(성공)

--- Level2_Programmers/기능개발.py
+++ Level2_Programmers/기능개발.py
@@ -1,17 +1,22 @@
+import math
 def solution(progresses, speeds):
-    answer = []
-    cnt = 1
-    speed = (100 - progresses[0]) // speeds[0]  # 첫번째 작업이 끝나는데 걸리는 일
+  answer = []
+  speed = math.ceil((100 - progresses[0]) / speeds[0])
+  complete = 1
+  
+  for i in range(1, len(progresses)):
+    curr = math.ceil((100 - progresses[i]) / speeds[i])
+    if speed >= curr:
+      complete += 1
+      
+    else:
+      speed = curr
+      answer.append(complete)
+      complete = 1
     
-    for i in range(1, len(progresses)):
-        if (speed >= (100 - progresses[i]) // speeds[i]):   # 다음 작업 속도가 더 느릴 경우, 이전 작업이 완료되면 같이 완료
-            cnt += 1
-        else:
-            answer.append(cnt)                              # 다음 작업 속도가 더 빠를 경우, 이전 작업 완료
-            speed = (100 - progresses[i]) // speeds[i]      # 다음 작업 속도로 걸리는 일 초기화
-            cnt = 1                                         # 완료 될 작업의 수 초기화
-    answer.append(cnt)                                      # 마지막 완료된 작업 추가
-    return answer
+  answer.append(complete)
+  
+  return answer
 
 
 # 입출력 예
