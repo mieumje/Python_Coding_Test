bc5fdfdb7000faf85c32ec54524a8bd2c8477cdb
mieumje <66072832+mieumje@users.noreply.github.com>
2022-09-26 19:51:34 +0900

    feat: n^2 배열 자르기(2차 실패)
    정확성  테스트
    테스트 1 〉	통과 (26.15ms, 16MB)
    테스트 2 〉	실패 (시간 초과)
    테스트 3 〉	실패 (시간 초과)
    테스트 4 〉	통과 (1.52ms, 10.3MB)
    테스트 5 〉	통과 (2.14ms, 10.2MB)
    테스트 6 〉	통과 (188.35ms, 50.9MB)
    테스트 7 〉	통과 (222.37ms, 53.1MB)
    테스트 8 〉	통과 (187.43ms, 53.4MB)
    테스트 9 〉	실패 (시간 초과)
    테스트 10 〉	실패 (시간 초과)
    테스트 11 〉	실패 (시간 초과)
    테스트 12 〉	실패 (시간 초과)
    테스트 13 〉	실패 (시간 초과)
    테스트 14 〉	실패 (시간 초과)
    테스트 15 〉	실패 (시간 초과)
    테스트 16 〉	실패 (시간 초과)
    테스트 17 〉	실패 (시간 초과)
    테스트 18 〉	실패 (시간 초과)
    테스트 19 〉	실패 (시간 초과)
    테스트 20 〉	실패 (시간 초과)
    채점 결과
    정확성: 30.0
    합계: 30.0 / 100.0

--- Level2_Programmers/n^2 배열 자르기.py
+++ Level2_Programmers/n^2 배열 자르기.py
@@ -1,16 +1,11 @@
 def solution(n, left, right):
   answer = []
-  arr = [[0] * (n + 1) for _ in range(n + 1)]
-  
-  for i in range(1, n + 1):
-    for j in range(1, n + 1):
-      maximum = max(i, j)
-      arr[i][j] = maximum
+  arr = [[max(i, j + 1) for i in range(n + 1)] for j in range(n + 1)]
  
-  for i in range(1, n + 1):
+  for i in range(n):
     answer += arr[i][1:]
 
-  answer = answer[left : right + 1]  
+  answer = answer[left : right + 1]
   
   return answer
 
@@ -18,8 +13,8 @@ def solution(n, left, right):
 # 3	  2	    5	      [3,2,2,3]
 # 4	  7	    14	    [4,3,3,3,4,4,4,4]
 
-n = 4
-left = 7
-right = 14
+n = 3
+left = 2
+right = 5
 
 print(solution(n, left, right))
