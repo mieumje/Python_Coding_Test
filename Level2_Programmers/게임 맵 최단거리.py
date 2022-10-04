022c3cc69de06b4fcbb8691694ec9dd9f2ee0521
mieumje <66072832+mieumje@users.noreply.github.com>
2022-10-04 22:29:04 +0900

    feat: 게임 맵 최단거리(3차 성공)

--- Level2_Programmers/게임 맵 최단거리.py
+++ Level2_Programmers/게임 맵 최단거리.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def solution(maps):
   answer = 0
   n, m = len(maps), len(maps[-1])
@@ -26,34 +28,36 @@ def solution(maps):
   answer = checked[-1][-1] if checked[-1][-1] != 1000001 else -1
   return answer
 
+from collections import deque
 def solution_bfs(maps):
-  from collections import deque
   answer = 0
   n, m = len(maps), len(maps[-1])
-  if n == 1 and m == 1:
-    return 1
-
-  visited = deque([(0, 0)])
-  checked = [[0] * n for _ in range(m)]
-  checked[0][0] = 1
   dx = [-1, 1, 0, 0]
-  dy = [0, 0, -1, 1]
+  dy = [0 ,0, -1, 1]
   
-  while visited:
-    x, y = visited.popleft()
-    
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-
-      if nx < 0 or ny < 0 or nx >= n or ny >= m:
-        continue
-      if maps[nx][ny] == 1 and checked[nx][ny] == 0:
-        checked[nx][ny] = checked[x][y] + 1
-        visited.append((nx, ny))
-    
+  def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+       
+    while queue:
+      x, y = queue.popleft()
+     
+      for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        
+        if nx < 0 or ny < 0 or nx >= n or ny >= m or maps[nx][ny] == 0:
+          continue
+        
+        if maps[nx][ny] == 1:
+          maps[nx][ny] = maps[x][y] + 1
+          queue.append((nx, ny))
+   
+    return maps[n - 1][m - 1]
   
-  answer = checked[-1][-1] if checked[-1][-1] != 0 else -1
+  answer = bfs(0, 0)
+  answer = -1 if answer == 1 else answer
+  
   return answer
 
 # maps	                                                        answer
