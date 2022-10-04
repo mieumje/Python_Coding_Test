0213411b3eeea63b218922bc038300aded0b227d
mieumje <66072832+mieumje@users.noreply.github.com>
2022-10-04 18:54:29 +0900

    feat: 게임 맵 최단거리(bfs, 효율성 및 런타임 실패)
    정확성  테스트
    테스트 1 〉	실패 (런타임 에러)
    테스트 2 〉	실패 (런타임 에러)
    테스트 3 〉	실패 (런타임 에러)
    테스트 4 〉	통과 (0.03ms, 10.1MB)
    테스트 5 〉	실패 (런타임 에러)
    테스트 6 〉	실패 (런타임 에러)
    테스트 7 〉	통과 (0.07ms, 10.2MB)
    테스트 8 〉	실패 (런타임 에러)
    테스트 9 〉	실패 (런타임 에러)
    테스트 10 〉	통과 (0.10ms, 10.2MB)
    테스트 11 〉	실패 (런타임 에러)
    테스트 12 〉	통과 (0.03ms, 10.2MB)
    테스트 13 〉	실패 (런타임 에러)
    테스트 14 〉	실패 (런타임 에러)
    테스트 15 〉	통과 (0.05ms, 10.2MB)
    테스트 16 〉	통과 (0.02ms, 10.2MB)
    테스트 17 〉	실패 (런타임 에러)
    테스트 18 〉	실패 (런타임 에러)
    테스트 19 〉	실패 (런타임 에러)
    테스트 20 〉	통과 (0.02ms, 10.3MB)
    테스트 21 〉	통과 (0.01ms, 10.4MB)
    효율성  테스트
    테스트 1 〉	통과 (13.00ms, 10.3MB)
    테스트 2 〉	실패 (런타임 에러)
    테스트 3 〉	통과 (8.22ms, 10.4MB)
    테스트 4 〉	통과 (5.29ms, 10.3MB)
    채점 결과
    정확성: 25.6
    효율성: 22.6
    합계: 48.1 / 100.0

--- Level2_Programmers/게임 맵 최단거리.py
+++ Level2_Programmers/게임 맵 최단거리.py
@@ -26,9 +26,40 @@ def solution(maps):
   answer = checked[-1][-1] if checked[-1][-1] != 1000001 else -1
   return answer
 
+def solution_bfs(maps):
+  from collections import deque
+  answer = 0
+  n, m = len(maps), len(maps[-1])
+  if n == 1 and m == 1:
+    return 1
+
+  visited = deque([(0, 0)])
+  checked = [[0] * n for _ in range(m)]
+  checked[0][0] = 1
+  dx = [-1, 1, 0, 0]
+  dy = [0, 0, -1, 1]
+  
+  while visited:
+    x, y = visited.popleft()
+    
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+
+      if nx < 0 or ny < 0 or nx >= n or ny >= m:
+        continue
+      if maps[nx][ny] == 1 and checked[nx][ny] == 0:
+        checked[nx][ny] = checked[x][y] + 1
+        visited.append((nx, ny))
+    
+  
+  answer = checked[-1][-1] if checked[-1][-1] != 0 else -1
+  return answer
+
 # maps	                                                        answer
 # [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]	11
 # [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]	-1
 
 maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
-print(solution(maps))
+# print(solution(maps))
+print(solution_bfs(maps))
