ac6cce17297ada8cb6d7d9f870f978a33a14a716
mieumje <66072832+mieumje@users.noreply.github.com>
2022-10-20 23:32:57 +0900

    feat: 가장 큰 정사각형 찾기(1차 실패)
    정확성  테스트
    테스트 1 〉	통과 (0.01ms, 10.4MB)
    테스트 2 〉	통과 (0.04ms, 10.2MB)
    테스트 3 〉	통과 (0.06ms, 10.2MB)
    테스트 4 〉	실패 (0.08ms, 10.2MB)
    테스트 5 〉	통과 (0.07ms, 10.2MB)
    테스트 6 〉	통과 (0.04ms, 10.2MB)
    테스트 7 〉	통과 (0.02ms, 10.3MB)
    테스트 8 〉	통과 (0.02ms, 10.2MB)
    테스트 9 〉	통과 (0.06ms, 10.2MB)
    테스트 10 〉	통과 (0.07ms, 10.2MB)
    테스트 11 〉	통과 (0.03ms, 10.2MB)
    테스트 12 〉	실패 (0.04ms, 10.3MB)
    테스트 13 〉	실패 (0.04ms, 10.3MB)
    테스트 14 〉	실패 (0.07ms, 10.2MB)
    테스트 15 〉	통과 (0.11ms, 10.4MB)
    테스트 16 〉	실패 (0.13ms, 10.2MB)
    테스트 17 〉	실패 (0.11ms, 10.2MB)
    테스트 18 〉	실패 (1.51ms, 10.2MB)
    테스트 19 〉	실패 (2.24ms, 10.2MB)
    효율성  테스트
    테스트 1 〉	통과 (568.40ms, 39.8MB)
    테스트 2 〉	실패 (649.26ms, 39.8MB)
    테스트 3 〉	실패 (606.07ms, 40MB)
    채점 결과
    정확성: 34.5
    효율성: 13.5
    합계: 48.0 / 100.0

---
+++ Level2_Programmers/가장 큰 정사각형 찾기.py
@@ -0,0 +1,27 @@
+def solution(board):
+  answer = 0
+  
+  dp = [[0 for _ in range(len(board[0]) + 1)] for _ in range(len(board) + 1)]
+  
+  for i in range(len(board[0])):
+    dp[1][i + 1] = board[0][i]
+  for i in range(len(board)):
+    dp[i + 1][1] = board[i][0]
+
+  for i in range(1, len(board) + 1):
+    for j in range(1, len(board[0]) + 1):
+      if board[i - 1][j - 1] == 1:
+        dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i - 1][j]) + 1
+        answer = max(answer, dp[i][j])
+  
+  answer *= answer
+  
+  return answer
+
+# board	                                    answer
+# [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]	9
+# [[0,0,1,1],[1,1,1,1]]	                    4
+
+board = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
+
+print(solution(board))
