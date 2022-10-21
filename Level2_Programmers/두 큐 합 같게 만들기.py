dd879ecde6a25a015f397fe52abe1c2fcb223fbd
mieumje <66072832+mieumje@users.noreply.github.com>
2022-10-21 17:07:44 +0900

    refactor: 두 큐 합 같게 만들기(2차 실패, 효율성 증가)
    queue1의 매번 구하지 않고 처음 합계를 구한 후 pop, insert에 따른 원소 삭제/추가의 경우만 계산
    
    TC 11, 12, 19, 20, 21, 29, 30 추가 성공
    
    정확성  테스트
    테스트 1 〉	실패 (0.01ms, 10.1MB)
    테스트 2 〉	통과 (0.01ms, 10.1MB)
    테스트 3 〉	통과 (0.00ms, 10MB)
    테스트 4 〉	통과 (0.01ms, 10.2MB)
    테스트 5 〉	통과 (0.02ms, 10.2MB)
    테스트 6 〉	통과 (0.04ms, 10.2MB)
    테스트 7 〉	통과 (0.02ms, 10.1MB)
    테스트 8 〉	통과 (0.02ms, 10.1MB)
    테스트 9 〉	통과 (0.07ms, 10MB)
    테스트 10 〉	통과 (0.13ms, 10.2MB)
    테스트 11 〉	통과 (910.57ms, 13.9MB)
    테스트 12 〉	통과 (187.95ms, 13.8MB)
    테스트 13 〉	통과 (7.31ms, 10.9MB)
    테스트 14 〉	통과 (6.82ms, 11.4MB)
    테스트 15 〉	통과 (250.30ms, 16.9MB)
    테스트 16 〉	통과 (32.88ms, 17.4MB)
    테스트 17 〉	통과 (8.71ms, 16.5MB)
    테스트 18 〉	통과 (91.13ms, 29.9MB)
    테스트 19 〉	통과 (912.98ms, 34.3MB)
    테스트 20 〉	통과 (7346.69ms, 34.7MB)
    테스트 21 〉	통과 (3033.01ms, 34.7MB)
    테스트 22 〉	실패 (시간 초과)
    테스트 23 〉	실패 (시간 초과)
    테스트 24 〉	실패 (시간 초과)
    테스트 25 〉	통과 (0.08ms, 10.1MB)
    테스트 26 〉	통과 (0.02ms, 10.1MB)
    테스트 27 〉	통과 (0.02ms, 10MB)
    테스트 28 〉	통과 (5821.23ms, 18.2MB)
    테스트 29 〉	통과 (0.16ms, 10.8MB)
    테스트 30 〉	통과 (5809.13ms, 18.1MB)
    채점 결과
    정확성: 86.7
    합계: 86.7 / 100.0

--- Level2_Programmers/두 큐 합 같게 만들기.py
+++ Level2_Programmers/두 큐 합 같게 만들기.py
@@ -3,17 +3,20 @@ def solution(queue1, queue2):
   hap = sum(queue1) + sum(queue2)
   target = hap // 2
   max_count = len(queue1) + len(queue2)
+  curr = sum(queue1)
   cnt = 0
   
   while cnt < max_count:
 
-    if sum(queue1) < target:
+    if curr < target:
+      curr += queue2[0]
       queue1.append(queue2.pop(0))
       cnt += 1
-    elif sum(queue1) > target:
+    elif curr > target:
+      curr -= queue1[0]
       queue2.append(queue1.pop(0))
       cnt += 1
-    elif sum(queue1) == target:
+    elif curr == target:
       return cnt
   
   return answer
@@ -23,7 +26,7 @@ def solution(queue1, queue2):
 # [1, 2, 1, 2]	[1, 10, 1, 2]	7
 # [1, 1]	      [1, 5]	      -1
 
-queue1 = [1, 1]
-queue2 = [1, 5]
+queue1 = [3, 2, 7, 2]
+queue2 = [4, 6, 5, 1]
 
 print(solution(queue1, queue2))
