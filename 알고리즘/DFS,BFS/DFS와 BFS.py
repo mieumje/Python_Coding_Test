1c31b6e5e8fc301fa52425abeda734c27d8ccfd1
mieumje <66072832+mieumje@users.noreply.github.com>
2022-10-22 16:46:59 +0900

    refactor: DFS와 BFS(BOJ, 2차 성공)

--- 알고리즘/DFS,BFS/DFS와 BFS.py
+++ 알고리즘/DFS,BFS/DFS와 BFS.py
@@ -3,39 +3,32 @@ from collections import defaultdict, deque
 
 N, M, V = map(int, input().split())
 
-arr = []
+g = [[0] * (N + 1) for _ in range(N + 1)]
 
 for _ in range(M):
   x, y = map(int, input().split())
-  arr.append([x, y])
-arr = sorted(arr, key = lambda x : (x[0], x[1]))
-print(arr)
-lists = defaultdict(list)  
-for i in arr:
-  x, y = i
-  lists[x].append(y)
-  lists[y].append(x)
-print(lists)
+  g[x][y] = 1
+  g[y][x] = 1
 
-def dfs(node, lists, visited):
+def dfs(node, visited):
   visited[node] = True
   print(node, end = ' ')
-  for i in lists[node]:
-    if not visited[i]:
-      dfs(i, lists, visited)
+  for i in range(len(g[node])):
+    if not visited[i] and g[node][i] == 1:
+      dfs(i, visited)
 
-def bfs(node, lists, visited):
+def bfs(node, visited):
   visited[node] = True
   q = deque([])
   q.append(node)
   while q:
     curr = q.popleft()
     print(curr, end = ' ')
-    for i in lists[curr]:
-      if not visited[i]:
+    for i in range(len(g[curr])):
+      if not visited[i] and g[curr][i] == 1:
         visited[i] = True
         q.append(i)
 
-dfs(V, lists, visited = [False] * (N + 1))
+dfs(V, visited = [False] * (N + 1))
 print('')     
-bfs(V, lists, visited = [False] * (N + 1))
+bfs(V, visited = [False] * (N + 1))
