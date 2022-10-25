23f065ced1de2ae0687244d1445e16e9c1acc708
mieumje <66072832+mieumje@users.noreply.github.com>
2022-10-25 16:25:39 +0900

    refactor: 조이스틱(테스트 케이스 추가, 기존 코드 변경)
    1. 현재 문자를 기준으로 'A'의 연속을 찾는다.
    2. 해당 연속된 'A'를 기준으로 탐색을 한다.
    3. 기존 방식(좌->우), 연속된'A'를 기준 왼쪽으로, 연속된'A'를 기준 오른쪽으로의 최솟값을 찾는다.
    4. 모든 문자열을 순회하며 가장 최솟값을 찾는다.
    5. 정답 =  각 문자의 상하 움직임 + 최소 움직임
    
    테스트 케이스 추가 후 기존 코드 정답
    정확성  테스트
    테스트 1 〉	통과 (0.02ms, 10.3MB)
    테스트 2 〉	통과 (0.02ms, 10MB)
    테스트 3 〉	통과 (0.01ms, 10.1MB)
    테스트 4 〉	통과 (0.03ms, 10.2MB)
    테스트 5 〉	통과 (0.02ms, 10.3MB)
    테스트 6 〉	통과 (0.02ms, 10.2MB)
    테스트 7 〉	통과 (0.02ms, 10.1MB)
    테스트 8 〉	통과 (0.01ms, 10.1MB)
    테스트 9 〉	통과 (0.02ms, 10MB)
    테스트 10 〉	통과 (0.01ms, 10.2MB)
    테스트 11 〉	통과 (0.02ms, 10.2MB)
    테스트 12 〉	통과 (0.01ms, 10.1MB)
    테스트 13 〉	실패 (0.04ms, 10.2MB)
    테스트 14 〉	통과 (0.02ms, 10.4MB)
    테스트 15 〉	통과 (0.03ms, 10.3MB)
    테스트 16 〉	통과 (0.00ms, 10.2MB)
    테스트 17 〉	통과 (0.01ms, 10.2MB)
    테스트 18 〉	실패 (0.02ms, 10.4MB)
    테스트 19 〉	통과 (0.01ms, 10.3MB)
    테스트 20 〉	통과 (0.03ms, 10MB)
    테스트 21 〉	통과 (0.01ms, 10MB)
    테스트 22 〉	실패 (0.02ms, 10.2MB)
    테스트 23 〉	실패 (0.03ms, 10.2MB)
    테스트 24 〉	실패 (0.01ms, 10.1MB)
    테스트 25 〉	실패 (0.03ms, 10.4MB)
    테스트 26 〉	통과 (0.01ms, 10.3MB)
    테스트 27 〉	실패 (0.02ms, 10.1MB)
    채점 결과
    정확성: 74.1
    합계: 74.1 / 100.0

--- Level2_Programmers/조이스틱.py
+++ Level2_Programmers/조이스틱.py
@@ -2,29 +2,21 @@ def solution(name):
     answer = 0
     tmp = [min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
            for char in name]
-
-    cur = 0
-    while True:
-        answer += tmp[cur]
-        tmp[cur] = 0
-        if sum(tmp) == 0:
-            break
-
-        left, right = 1, 1
-        while tmp[cur - left] == 0:
-            left += 1
-        while tmp[cur + right] == 0:
-            right += 1
-
-        answer += left if left < right else right
-        cur += -left if left < right else right
-
+    answer = sum(tmp)
+    min_move = len(name) - 1
+    for i, _ in enumerate(name):
+        next = i + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+        min_move = min([min_move, i * 2 + len(name) - next, i + 2 * (len(name) - next)])
+    answer += min_move
     return answer
 
-
 # 입출력 예
-# name	    return
-# "JEROEN"	56
-# "JAN"	    23
-name = "JEROEN"
+# name	        return
+# "JEROEN"	    56
+# "JAN"	        23
+# "ABABAAAAABA" 10
+
+name = "JAN"
 print(solution(name))
