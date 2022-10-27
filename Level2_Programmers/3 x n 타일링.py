6f3c961938fc3c15e1b2102c4e4ec23fe7fd8108
mieumje <66072832+mieumje@users.noreply.github.com>
2022-10-27 18:26:40 +0900

    refactor: 3 x n 타일링(dp, 점화식 수정)
    점화식
    f(n) = f(n - 2) * 4 - f(n - 4)

--- Level2_Programmers/3 x n 타일링.py
+++ Level2_Programmers/3 x n 타일링.py
@@ -26,7 +26,27 @@ def solution(n):
   
   return answer
 
+def solution2(n):
+  answer = 0
+  if n % 2 == 1: 
+    return 0
+  dp = [None] * 5001
+  dp[2] = 3
+  dp[4] = 11
+  
+  if n == 2 or n == 4:
+    return dp[n]
+  
+  for i in range(6, n + 1):
+    if i % 2 == 1: continue
+    dp[i] = (dp[i - 2] * 4 - dp[i - 4]) % 1000000007
+  
+  answer = dp[n]
+  return answer
+  
 # n	result
 # 4	11
+# 8 153
 
 print(solution(n=8))
+print(solution2(n=8))
