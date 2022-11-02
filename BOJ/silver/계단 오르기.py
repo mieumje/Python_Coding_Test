ea39628c20997bad51de436054ddd598952f65e0
mieumje <66072832+mieumje@users.noreply.github.com>
2022-11-02 15:20:37 +0900

    refactor: 계단 오르기(BOJ, 2차 성공)
    append(), pop()으로 런타임 에러 해결

--- BOJ/silver/계단 오르기.py
+++ BOJ/silver/계단 오르기.py
@@ -1,12 +1,15 @@
 N = int(input())
-arr = [0]
+arr = []
+dp = []
 for _ in range(N):
   arr.append(int(input()))
-dp = [0] * (N + 1)
-dp[1] = arr[1]
-dp[2] = max(arr[2], dp[1] + arr[2])
 
-for i in range(3, N + 1):
-  dp[i] = max(arr[i] + dp[i - 2], arr[i] + arr[i - 1] + dp[i - 3])
-  
-print(dp[-1])
+if N <= 2:
+  print(sum(arr))
+else:
+  dp.append(arr[0])
+  dp.append(max(arr[1], arr[0] + arr[1]))
+  dp.append(max(arr[2] + arr[1], arr[2] + arr[0]))
+  for i in range(3, N):
+    dp.append(max(arr[i] + dp[i - 2], arr[i] + arr[i - 1] + dp[i - 3]))
+  print(dp.pop())
