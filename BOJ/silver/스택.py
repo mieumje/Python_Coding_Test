f6c05755058df7b7fe8ebb8788c269bf80981a31
mieumje <66072832+mieumje@users.noreply.github.com>
2022-11-05 16:01:26 +0900

    feat: 스택(BOJ, 시간초과 실패)

---
+++ BOJ/silver/스택.py
@@ -0,0 +1,29 @@
+import sys
+
+N = int(input())
+
+stack = []
+
+for _ in range(N):
+  inputs = input().split()
+  order = inputs[0]
+  
+  if order == 'push':
+    stack.append(inputs[1])
+  elif order == 'pop':
+    if len(stack) == 0:
+      print(-1)
+    else:
+      print(stack.pop())
+  elif order == 'size':
+    print(len(stack))
+  elif order == 'empty':
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+  elif order == 'top':
+    if len(stack) == 0:
+      print(-1)
+    else:
+      print(stack[-1])
